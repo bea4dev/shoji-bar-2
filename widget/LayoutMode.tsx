98353fdcbb43f79e9e92489ff7038141a109ddbc
bea4dev <bea0224@outlook.jp>
2026-06-05 23:49:23 +0900

    add LayoutMode Selector click

--- widget/LayoutMode.tsx
+++ widget/LayoutMode.tsx
@@ -1,9 +1,10 @@
 import { Gdk, Gtk } from "ags/gtk4"
 import { createComputed } from "gnim"
-import { view, monitorView, activeWorkspace } from "../utils/workspaceState"
+import { view, monitorView, activeWorkspace, ipc } from "../utils/workspaceState"
 
 // 現在のワークスペースがタイル型かフローティング型かを表示するラベル。
 // "Tiled" / "Float"(monospace)とアイコン(assets/tiled.svg, assets/float.svg)。
+// クリックでそのモニタの現在ワークスペースのタイル/フロート切替を要求する。
 export function LayoutMode({ gdkmonitor }: { gdkmonitor: Gdk.Monitor }) {
   const connector = gdkmonitor.get_connector()
 
@@ -13,20 +14,28 @@ export function LayoutMode({ gdkmonitor }: { gdkmonitor: Gdk.Monitor }) {
   })
 
   return (
-    <box
+    <button
       cssName="LayoutMode"
       halign={Gtk.Align.START}
       valign={Gtk.Align.CENTER}
+      onClicked={() =>
+        ipc.send(
+          "workspaces.toggleTiling",
+          connector ? { monitor: connector } : undefined,
+        )
+      }
     >
-      <image
-        cssName="LayoutModeIcon"
-        file={tiled((t) => `${SRC}/assets/${t ? "tiled" : "float"}.svg`)}
-        pixelSize={16}
-      />
-      <label
-        cssName="LayoutModeLabel"
-        label={tiled((t) => (t ? "Tiled" : "Float"))}
-      />
-    </box>
+      <box>
+        <image
+          cssName="LayoutModeIcon"
+          file={tiled((t) => `${SRC}/assets/${t ? "tiled" : "float"}.svg`)}
+          pixelSize={16}
+        />
+        <label
+          cssName="LayoutModeLabel"
+          label={tiled((t) => (t ? "Tiled" : "Float"))}
+        />
+      </box>
+    </button>
   )
 }
